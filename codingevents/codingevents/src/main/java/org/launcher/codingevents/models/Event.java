2beaa0965d919f647b81d6ea002763f5e1432e94
bevdbx <bevdbx607@gmail.com>
2023-12-20 09:07:27 +0200

    pentru coding events e parcurs de la tutorialul 10 pana la 31 inclusiv

--- codingevents/codingevents/src/main/java/org/launcher/codingevents/models/Event.java
+++ codingevents/codingevents/src/main/java/org/launcher/codingevents/models/Event.java
@@ -1,20 +1,43 @@
 package org.launcher.codingevents.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
+@Entity
+public class Event extends AbstractEntity{
 
-public class Event {
-
-    private int id;
-    private static int nextId = 1;
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters!")
     private String name;
+
+    @Size(max = 500, message = "Description must be max 500 characters!")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email. Try again!")
+    private String contactEmail;
+
+    @ManyToOne
+    @NotNull(message = "Category is required.")
+    private EventCategory eventCategory;
+
+
+    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
         this.name = name;
         this.description = description;
-        this.id = nextId;
-        nextId++;
+        this.contactEmail = contactEmail;
+        this.eventCategory = eventCategory;
     }
+
+    public Event() {}
 
     public String getName() {
         return name;
@@ -32,25 +55,24 @@ public class Event {
         this.description = description;
     }
 
-    public int getId() {
-        return id;
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public EventCategory getEventCategory() {
+        return eventCategory;
+    }
+
+    public void setEventCategory(EventCategory eventCategory) {
+        this.eventCategory = eventCategory;
     }
 
     @Override
     public String toString() {
-        return name + ' ' + description + ' ' + id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
+        return name + ' ' + description + ' ' + contactEmail;
     }
 }
