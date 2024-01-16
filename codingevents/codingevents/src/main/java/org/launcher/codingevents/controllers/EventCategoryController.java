347b5c1be23198104c319aa37d1b51b6268bf135
bevdbx <bevdbx607@gmail.com>
2024-01-16 23:37:29 +0200

    Tutoriale Spring 35/35

--- codingevents/codingevents/src/main/java/org/launcher/codingevents/controllers/EventCategoryController.java
+++ codingevents/codingevents/src/main/java/org/launcher/codingevents/controllers/EventCategoryController.java
@@ -40,6 +40,4 @@ public class EventCategoryController {
         eventCategoryRepository.save(newEvent);
         return "redirect:/categories";
     }
-
-
 }
--- codingevents/codingevents/src/main/java/org/launcher/codingevents/models/Event.java
+++ codingevents/codingevents/src/main/java/org/launcher/codingevents/models/Event.java
@@ -1,16 +1,15 @@
 package org.launcher.codingevents.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.Email;
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Event extends AbstractEntity{
 
@@ -18,22 +17,20 @@ public class Event extends AbstractEntity{
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters!")
     private String name;
 
-    @Size(max = 500, message = "Description must be max 500 characters!")
-    private String description;
-
-    @NotBlank(message = "Email is required.")
-    @Email(message = "Invalid email. Try again!")
-    private String contactEmail;
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
 
     @ManyToOne
     @NotNull(message = "Category is required.")
     private EventCategory eventCategory;
 
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
 
     public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
-        this.contactEmail = contactEmail;
         this.eventCategory = eventCategory;
     }
 
@@ -47,22 +44,6 @@ public class Event extends AbstractEntity{
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
     public EventCategory getEventCategory() {
         return eventCategory;
     }
@@ -71,8 +52,24 @@ public class Event extends AbstractEntity{
         this.eventCategory = eventCategory;
     }
 
+    public EventDetails getEventDetails() {
+        return eventDetails;
+    }
+
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
+    }
+
     @Override
     public String toString() {
-        return name + ' ' + description + ' ' + contactEmail;
+        return name;
     }
 }
